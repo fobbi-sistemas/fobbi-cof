57c4775f5614fee2741736c6691c8378e76f6513
Alisson Hoepers <alisson@hsys.dev.br>
2024-08-15 14:23:27 -0300

    .

--- pages/header.php
+++ pages/header.php
@@ -20,7 +20,7 @@
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
 	
 	<!-- geral css -->
-    <link rel="stylesheet" href="../../../../assets/css/style.css?v=1"/>
+    <link rel="stylesheet" href="../../../../assets/css/style.css?v=2"/>
     
     <!-- editor code css -->
     <link rel="stylesheet" href="../../../../assets/css/editor-code.css"/>
